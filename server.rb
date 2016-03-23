240253447051d937ea73cd77b8816093076eb533
Lincoln Kupke <lincolnkupke@gmail.com>
2016-03-23 18:31:23 -0400

    actor page selection/movie order

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "pg"
 require "erb"
+require "pry"
 
 configure :development do
   set :db_config, { dbname: "movies" }
@@ -21,9 +22,21 @@ end
 
 
 get '/movies' do
+  order = "movies.title"
+  if params["Order"]
+    order = "movies.#{params['Order']}"
+  end
+
+  sql = <<-SQL
+    SELECT movies.id, movies.title, movies.year, movies.rating, genres.name, studios.name
+      FROM movies LEFT OUTER JOIN studios ON (movies.studio_id = studios.id)
+      LEFT OUTER JOIN genres ON (movies.genre_id = genres.id)
+      ORDER BY #{order};
+  SQL
 
   db_connection do |conn|
-    @movies = conn.exec("SELECT movies.id, movies.title, movies.year, movies.rating, genres.name, studios.name FROM movies LEFT OUTER JOIN studios ON (movies.studio_id = studios.id) LEFT OUTER JOIN genres ON (movies.genre_id = genres.id) ORDER BY movies.title;")
+    @movies = conn.exec(sql)
+    # binding.pry
   end
 
   erb :'movies/index'
@@ -32,18 +45,54 @@ end
 get '/movies/:id' do
 
   db_connection do |conn|
-    @movie_info = conn.exec_params("SELECT movies.title, movies.year, movies.rating, genres.name, studios.name FROM movies LEFT OUTER JOIN studios ON (movies.studio_id = studios.id) LEFT OUTER JOIN genres ON (movies.genre_id = genres.id) WHERE movies.id = $1;", [params[:id]])
-    @actor_list = conn.exec_params("SELECT cast_members.character, actors.name, actors.id FROM cast_members JOIN actors ON (cast_members.actor_id = actors.id) WHERE cast_members.movie_id = $1;", [params[:id]])
+    sql = <<-SQL
+      SELECT movies.title, movies.year, movies.rating, genres.name, studios.name
+      FROM movies LEFT OUTER JOIN studios ON (movies.studio_id = studios.id)
+      LEFT OUTER JOIN genres ON (movies.genre_id = genres.id)
+      WHERE movies.id = $1;
+    SQL
+
+    sql2 = <<-SQL
+      SELECT cast_members.character, actors.name, actors.id
+        FROM cast_members JOIN actors ON (cast_members.actor_id = actors.id)
+        WHERE cast_members.movie_id = $1;
+    SQL
+    @movie_info = conn.exec_params(sql, [params[:id]])
+    @actor_list = conn.exec_params(sql2, [params[:id]])
   end
 
   erb :'movies/show'
 end
 
 get '/actors' do
-
-  db_connection do |conn|
-    @results = conn.exec("SELECT id, name FROM actors ORDER BY name;")
+  @page = 0
+  if params["page"] == "0"
+    @page = 0
+  elsif params["page"] == "1"
+    @page = 1
+  elsif
+    @page = params["page"].to_i
   end
+
+  sql = <<-SQL
+    SELECT id, name FROM actors
+    ORDER BY name LIMIT 20
+    OFFSET $1;
+  SQL
+  db_connection do |conn|
+    @total_amount = conn.exec("SELECT COUNT(name) FROM actors;")
+
+    @results = conn.exec_params(sql,[@page])
+  end
+  @total_amount = @total_amount.values.flatten!.first.to_i
+  @amount_actor_pgs = @total_amount/ 20
+
+  # db_connection do |conn|
+  #
+  #
+  #
+  #   @results = conn.exec("SELECT id, name FROM actors ORDER BY name ;")
+  # end
 
   erb :'actors/index'
 end
